98ef5ede941a53c7d6786abbe2cd7743f30a3125
文耀 赵 <2648172780@qq.com>
2025-11-04 00:05:28 +0800

    feat: Http Code

--- RESTfulAPI/Controllers/TouristRoutesController.cs
+++ RESTfulAPI/Controllers/TouristRoutesController.cs
@@ -14,10 +14,24 @@ namespace RESTfulAPI.Controllers
         {
             _touristRouteRepository = touristRouteRepository;
         }
-
+        [HttpGet]
         public IActionResult GerTouristRoutes()
         {
             var routes = _touristRouteRepository.GetTouristRoutes();
+            if(routes == null || routes.Count() <= 0)
+            {
+                return NotFound("没有旅游路线");
+            }
+            return Ok(routes);
+        }
+        [HttpGet("{touristRouteId}")]
+        public IActionResult GetTouristRoutesbyId(Guid touristRouteId)
+        {
+            var routes = _touristRouteRepository.GetTouristRouteByID(touristRouteId);
+            if (routes == null)
+            {
+                return NotFound("没有旅游路线");
+            }
             return Ok(routes);
         }
     }
